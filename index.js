9bb543a3b498c769a9913849e3d9b49694951d4b
Chloe Steffens <caforonda103@yahoo.com>
2020-10-10 03:52:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var driver = {}
 
-function updateDriverWithKeyAndValue(driver,object, key, value) {
+function updateDriverWithKeyAndValue(driver, object, key, value) {
   return Object.assign({}, object, { [key]: value })
 }
 
-function destructivelyUpdateDriverWithKeyAndValue(driver, object, key) {
+function destructivelyUpdateDriverWithKeyAndValue(driver, object, key, value) {
   object[key] = value
   return object
 }
